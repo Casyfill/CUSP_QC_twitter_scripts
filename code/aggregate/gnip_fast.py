8dd4bff0b9b771707f89e09dcb70952465565834
Philipp Kats <casyfill@gmail.com>
2016-02-19 19:32:18 -0500

    Mgnip_aggregator

--- code/aggregate/gnip_fast.py
+++ code/aggregate/gnip_fast.py
@@ -14,43 +14,48 @@ import json
 
 walk_dir = sys.argv[1]
 
-import os 
+import os
+
 
 def getAllFilesRec(path, Format=None):
-	'''returns all files in all folders inside specified of selected format'''
+    '''returns all files in all folders inside specified of selected format'''
 
-	result = []
-	for root, subdirs, files in os.walk(walk_dir):
-		for filename in files: 
-			if Format==None or filename.endswith(Format):
-				result.append(os.path.join(root, filename))
-			else:
-				pass
+    for root, subdirs, files in os.walk(walk_dir):
+        for filename in files:
+            if Format == None or filename.endswith(Format):
+                yield os.path.join(root, filename)
+            else:
+                pass
 
-	return result
 
 def parseTweet(tweet, source):
+    if 'geo' in tweet.keys():
+        lat, lon = tweet['geo']['coordinates'][
+            0], tweet['geo']['coordinates'][1]
+    else:
+        lat, lon = None, None
+
     return (int(tweet['id'].split(':')[-1]),
-                    int(dateutil.parser.parse(tweet['postedTime']).strftime("%s")), # UTF TIME UNIX TIMESTAMP
-                    tweet['geo']['coordinates'][0],
-                    tweet['geo']['coordinates'][1],
-                    tweet['body'],
-                    int(tweet['actor']['id'].split(':')[-1]),
-                    tweet['retweetCount'],
-                    tweet['favoritesCount'],
-                    tweet['generator']['displayName'],
-                    source)
+            # UTF TIME UNIX TIMESTAMP
+            int(dateutil.parser.parse(tweet['postedTime']).strftime("%s")),
+            lat,
+            lon,
+            tweet['body'],
+            int(tweet['actor']['id'].split(':')[-1]),
+    		int(tweet['retweetCount']),
+            tweet.get('favoritesCount', None),
+            tweet['generator']['displayName'],
+            source)
 
 
 def parseJsonArrayFile(path, dbPath):
-	source = path.split('/')[-2]   
-	conn = sqlite3.connect(dbPath)
-	c = conn.cursor()
-	
+    source = 'M'
+    conn = sqlite3.connect(dbPath)
+    c = conn.cursor()
 
-	tweetE = '''CREATE TABLE IF NOT EXISTS tweets (id INTEGER PRIMARY KEY, 
+    tweetE = '''CREATE TABLE IF NOT EXISTS tweets (id INTEGER PRIMARY KEY, 
 		                                               timestamp INTEGER, 
-		                                               lon REAL, lat REAL, 
+		                                               lat REAL, lon REAL, 
 		                                               tweet TEXT, 
 		                                               user_id INTEGER, 
 		                                               rtwts INTEGER,
@@ -58,31 +63,38 @@ def parseJsonArrayFile(path, dbPath):
 		                                               application TEXT,
 	                                                   source TEXT)'''
 
-	c.execute(tweetE)
+    c.execute(tweetE)
 
-	with gzip.open(path, 'rb') as f:
-		lines = f.read().split('\r')
+    with gzip.open(path, 'rb') as f:
+        lines = f.read().split('\r')
 
-		for line in lines:
-			if 'geo' in line:
-				try:
-					t = parseTweet(json.loads(line.strip()), source )
-					c.execute('INSERT OR IGNORE INTO tweets VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', t )
-					conn.commit()
+        for line in lines[:-2]:
+        	if len(line)>2:
+	            try:
+	                t = parseTweet(json.loads(line.strip()), source)
+	                c.execute(
+	                    'INSERT OR IGNORE INTO tweets VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', t)
+	                conn.commit()
+	            except Exception, e:
+	            	print type(e), str(e)
+	            	print line
+	            	pass
+	        else:
+	        	pass
+    conn.close()
 
-				except Exception, e:
-					pass
-	conn.close()
 
-def main(walk_dir, dbPath, ID):
-	dbPath += '/%s.db' % ID
-	try:
-		for file_path in getAllFilesRec(walk_dir, Format='.json.gz'):
-			parseJsonArrayFile(file_path, dbPath)
-			print '.'
-	except Exception, e:
-		print str(e)
+def main(walk_dir):
+    dbPath = 'martin_db.db'
+
+    for file_path in getAllFilesRec(walk_dir, Format='.json.gz'):
+        try:
+            parseJsonArrayFile(file_path, dbPath)
+            print file_path
+        except Exception, e:
+            print str(e)
 
 
 if __name__ == '__main__':
-	main(sys.argv[1], sys.argv[2], sys.argv[3])
+    main(sys.argv[1])
+    print 'done!!!!!!!!'
