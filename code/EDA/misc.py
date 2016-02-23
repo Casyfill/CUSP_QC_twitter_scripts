34cf916f3012599ba2a2977b11e7590413ee6bc6
Philipp Kats <casyfill@gmail.com>
2016-02-23 17:47:33 -0500

    Report 2
    - clustering
    - network1
    - big data
    
    #8yKreFj5

--- code/EDA/misc.py
+++ code/EDA/misc.py
@@ -5,7 +5,31 @@ import sqlite3
 import pandas as pd
 import geopandas as gp
 from shapely.geometry import Point
+import os
 
+
+def getFiles(folder, format, full=True, filtr=None):
+    for f in os.listdir(folder):
+        if f.endswith(format):
+            if filtr==None or filtr not in f:
+                if full:
+                    yield os.path.join(folder, f)
+                else:
+                    yield f
+
+
+def mostRecentFile(path):
+    '''returns most recent .db file in folder'''
+
+    def reduceRecent(current, newPath):
+        d = os.stat(newPath).st_ctime
+        if d > current[1]:
+            return (newPath,d)
+
+
+    path, time = reduce(reduceRecent, getFiles(path, 'db'), (None, 0))
+
+    return  path
 
 def getSQLiteTweets(path, cols):
     '''gets all data as tuple'''
@@ -17,10 +41,16 @@ def getSQLiteTweets(path, cols):
     return data
 
 def getDF(path):
-    columns = ('id','timestamp','lon', 'lat', 'tweet', 'user_id', 'rtwts', 'fvrts', 'application', 'source')
-    data = getSQLiteTweets(path, columns)
     
-    return pd.DataFrame(data, columns=columns )
+    engine = create_engine('sqlite:///%s' % path)
+    columns = ('id','timestamp','lon', 'lat', 'tweet', 'user_id', 'rtwts', 'fvrts', 'application', 'source')
+
+    with engine.connect() as conn, conn.begin():
+        data = pd.read_sql_table('tweets', conn, columns=columns)
+
+    data['ts'] = pd.to_datetime(data.timestamp, unit='s') - datetime.timedelta(hours=5) ## UTS - 5h NYC
+    return misc.toGeoDataFrame(data, lat='lat',lon='lon')  
+
 
 
 def toGeoDataFrame(df, lat='Latitude',lon='Longitude'):
--- code/aggregate/sJoinZips.py
+++ code/aggregate/sJoinZips.py
@@ -4,39 +4,53 @@
 
 import pandas as pd
 import geopandas as gp
-from shapely.geometry import Point
-from geopandas.tools import sjoin
+from sqlalchemy import create_engine
 
-
-def getSQLiteTweets(path):
-    '''gets all data as tuple'''
-    conn = sqlite3.connect(path)
-    data = conn.cursor().execute('SELECT * FROM tweets').fetchall() ## all but raw data
-    conn.close()
-    return data
+from misc import toGeoDataFrame
 
 def getDF(path):
-    data = getSQLiteTweets(path)
-    columns = ('id','timestamp','lon', 'lat', 'tweet', 'user_id', 'rtwts', 'fvrts', 'application', 'source')
-    
-    return pd.DataFrame(data, columns=columns )
-    
+	'''return geodataframe'''
+	# FIXIT: repair timestamp parsing
 
-def toGeoDataFrame(df, lat='Latitude',lon='Longitude'):
-    '''dataframe to geodataframe'''
-    df['geometry'] = df.apply(lambda z: Point(z[lon], z[lat]), axis=1)
-    df = gp.GeoDataFrame(df)
-    df.crs = {'init': 'epsg:4326', 'no_defs': True}
-    return df 
+	engine = create_engine('sqlite:///%s' % path)
+	columns = ('id','timestamp','lon', 'lat', 'tweet', 'user_id', 'rtwts', 'fvrts', 'application', 'source')
 
-def sJoinZips(path):
-	'''give a zip attribute for each tweet'''
-	zipPath = 'zips.json'
-	gdf = toGeoDataFrame(getDF(path), lat='lon',lon='lat')
+	with engine.connect() as conn, conn.begin():
+	    data = pd.read_sql_table('tweets', conn, columns=columns)
+
+	data['ts'] = pd.to_datetime(data.timestamp, unit='s') - datetime.timedelta(hours=5) ## UTS - 5h NYC
+	return misc.toGeoDataFrame(data, lat='lat',lon='lon')  
+
+
+def sJOINT(gdf):
+	'''attaches zipcode label to each point'''
+
+	zipPath = '/misk/zipcode_tabular.geojson'
 	zips = gp.read_file(zipPath)[['geometry', 'postalCode']]
 
-	save_to_DB
+	return sjoin(gdf, zips, how="left", op='within')
 
-if __name__ == '__main__':
-		sJoinZips()	
+def writeSQL(gdf, path):
+	dbPath = path.replace('.db','_zips.db')
+	engine = create_engine('sqlite:///%s' % dbPath)
+	columns = ('id','timestamp','lon', 'lat', 'tweet', 'user_id', 'rtwts', 'fvrts', 'application', 'source')
 
+	with engine.connect() as conn, conn.begin():
+	gdf[[x for x in gdf.columns.tolist() if x!='geometry']].to_sql('tweets', con, flavor='sqlite', schema=None, if_exists='fail')
+	
+	print 'Done!\n\n', dbPath
+
+
+def main(path):
+	
+	gdf = getDF(path)
+	result = sJOINT(gdf)
+	writeSQL(result, path)
+
+if __name__=='__main__':
+    if len(sys.argv)<2:
+        sys.stderr.write('USAGE: python %s <DATABASE FILE>\n' % sys.argv[0])
+        sys.exit(1)
+        
+        main(sys.argv[1])
+        
